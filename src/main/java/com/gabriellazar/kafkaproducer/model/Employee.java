72c8fb394baa4444299db1fa4fea91ab861a574b
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-08-09 11:12:39 -0500

    repository interface

--- src/main/java/com/gabriellazar/kafkaproducer/model/Employee.java
+++ src/main/java/com/gabriellazar/kafkaproducer/model/Employee.java
@@ -1,12 +1,20 @@
 package com.gabriellazar.kafkaproducer.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 @Entity
+@Table(name = "employees")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,7 +23,7 @@ public class Employee {
     private String lastName;
     private String email;
     private String phoneNumber;
-    private String hireDate;
+    private LocalDate hireDate;
     private Long jobId;
     private BigDecimal salary;
     private Long managerId;
