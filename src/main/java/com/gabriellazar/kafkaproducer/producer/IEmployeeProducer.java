fbfc85a1df3ae52f0e879b9c258aa2530fd810ff
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-08-10 23:14:00 -0500

    kafka producer implementation

--- src/main/java/com/gabriellazar/kafkaproducer/producer/IEmployeeProducer.java
+++ src/main/java/com/gabriellazar/kafkaproducer/producer/IEmployeeProducer.java
@@ -1,4 +1,7 @@
 package com.gabriellazar.kafkaproducer.producer;
 
+import com.gabriellazar.kafkaproducer.model.Employee;
+
 public interface IEmployeeProducer {
+    void sendMessage(Employee employee);
 }
--- src/main/java/com/gabriellazar/kafkaproducer/producer/impl/EmployeeProducerImplementation.java
+++ src/main/java/com/gabriellazar/kafkaproducer/producer/impl/EmployeeProducerImplementation.java
@@ -1,4 +1,43 @@
 package com.gabriellazar.kafkaproducer.producer.impl;
 
-public class EmployeeProducerImplementation {
+import com.gabriellazar.kafkaproducer.model.Employee;
+import com.gabriellazar.kafkaproducer.producer.IEmployeeProducer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+public class EmployeeProducerImplementation implements IEmployeeProducer {
+
+    private KafkaTemplate<String, Employee> kafkaTemplate;
+
+    @Value("${spring.kafka.template.default-topic}")
+    private String topicName;
+
+    public EmployeeProducerImplementation(KafkaTemplate<String, Employee> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void sendMessage(Employee employee) {
+
+        if(employee.getFirstName() != null && employee.getLastName() != null){
+            String employeeKey = employee.getFirstName() + " " + employee.getLastName();
+            ListenableFuture<SendResult<String,Employee>> employeeFuture = kafkaTemplate.send(topicName,employeeKey,employee);
+
+            employeeFuture.addCallback(new ListenableFutureCallback<SendResult<String, Employee>>() {
+                @Override
+                public void onFailure(Throwable throwable) {
+
+                }
+
+                @Override
+                public void onSuccess(SendResult<String, Employee> stringEmployeeSendResult) {
+
+                }
+            });
+        }
+
+    }
 }
