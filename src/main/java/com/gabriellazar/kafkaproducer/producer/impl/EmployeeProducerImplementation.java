0cfd9dffceb2363ec6f7b422c9e6991ab2e7fe36
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-08-22 05:22:51 -0500

    adding logs to project

--- src/main/java/com/gabriellazar/kafkaproducer/producer/impl/EmployeeProducerImplementation.java
+++ src/main/java/com/gabriellazar/kafkaproducer/producer/impl/EmployeeProducerImplementation.java
@@ -2,12 +2,14 @@ package com.gabriellazar.kafkaproducer.producer.impl;
 
 import com.gabriellazar.kafkaproducer.model.Employee;
 import com.gabriellazar.kafkaproducer.producer.IEmployeeProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+@Slf4j
 public class EmployeeProducerImplementation implements IEmployeeProducer {
 
     private KafkaTemplate<String, Employee> kafkaTemplate;
@@ -22,21 +24,23 @@ public class EmployeeProducerImplementation implements IEmployeeProducer {
     @Override
     public void sendMessage(Employee employee) {
 
-        if(employee.getFirstName() != null && employee.getLastName() != null){
+        if (employee.getFirstName() != null && employee.getLastName() != null) {
             String employeeKey = employee.getFirstName() + " " + employee.getLastName();
-            ListenableFuture<SendResult<String,Employee>> employeeFuture = kafkaTemplate.send(topicName,employeeKey,employee);
+            ListenableFuture<SendResult<String, Employee>> employeeFuture = kafkaTemplate.send(topicName, employeeKey, employee);
 
             employeeFuture.addCallback(new ListenableFutureCallback<SendResult<String, Employee>>() {
                 @Override
                 public void onFailure(Throwable throwable) {
-
+                    log.error("Unable to deliver message [{}]. {}", employee, throwable.getMessage());
                 }
 
                 @Override
                 public void onSuccess(SendResult<String, Employee> stringEmployeeSendResult) {
-
+                    log.info("Successfully send employee :: {}", employee);
                 }
             });
+        } else {
+            log.warn("Employee firstName or lastName is null :: {}", employee);
         }
 
     }
